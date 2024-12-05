608a951fe8636c8dfbce1e90509d979a237248a4
Fakhridanish mirza Priyoyuwono <fakhridanish.mirza@gmail.com>
2024-12-05 17:14:29 +0700

    Deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: false, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/fakhridanishmp.github.io/' : '',
+  basePath: isProd ? '/fakhridanishmp.github.io' : '',
+  output: 'export'
 };
 
 export default nextConfig;
