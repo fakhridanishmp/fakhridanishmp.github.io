73a0017e537c9856ea03658f5134ab4a35451160
Fakhridanish mirza Priyoyuwono <fakhridanish.mirza@gmail.com>
2024-12-05 18:07:34 +0700

    Small update

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
 import Header from '../components/headers';
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-neutral-800">
       <Header />
-      <div className="text-white h-screen pt-40 md:pl-32 px-8 font-mono">
+
+      {/* About Section */}
+      <div id="about" className="text-white h-screen pt-40 md:pt-52 md:pl-32 px-8 font-mono">
         <p className="text-lg text-sky-400">Hi, my name is</p>
         <p className="text-5xl font-bold pt-6">Fakhridanish Mirza</p>
         <p className="text-5xl font-bold">Priyoyuwono</p>
         <p className="text-lg text-left max-w-2xl pt-10">I am a student at SMA Negeri 1 Waru, and I am interested in data science and electrical engineering, with a focus on the development of IoT.</p>
       </div>
 
-      <div className="min-h-screen bg-neutral-800">
+      {/* Projects Section */}
+      <div id="projects" className="min-h-screen bg-neutral-800">
         <div className="flex justify-center pt-14 flex-col items-center">
           <p className="text-white text-5xl font-bold font-mono pr-4">PROJECTS</p>
           <p className="text-white text-md font-bold font-mono mt-4">Here are some of my projects</p>
@@ -37,34 +40,35 @@ export default function Home() {
             <p className="text-md pt-4">The Non-Invasive Glucometer is a blood sugar monitoring device developed to address the increasing cases of diabetes mellitus, particularly among children in Indonesia. Many children are afraid of needles and are reluctant to regularly check their blood sugar levels. This device offers a more comfortable and painless way to measure blood sugar, making it easier to monitor diabetes in children.</p>
           </div>
         </div>
+      </div>
 
-        <div className="min-h-screen bg-neutral-800 pt-32 pb-8">
-          <div className="flex justify-center flex-col items-center">
-            <p className="text-white text-5xl font-bold font-mono">ACHIEVEMENTS</p>
-            <p className="text-white text-md font-bold font-mono mt-4">Here are some of the notable awards and recognitions I’ve received for my projects</p>
+      {/* Achievements Section */}
+      <div id="achievements" className="min-h-screen bg-neutral-800 pt-32 pb-8">
+        <div className="flex justify-center flex-col items-center">
+          <p className="text-white text-5xl font-bold font-mono">ACHIEVEMENTS</p>
+          <p className="text-white text-md font-bold font-mono mt-4">Here are some of the notable awards and recognitions I’ve received for my projects</p>
+        </div>
+
+        <div className="flex flex-col items-start justify-center mt-12 px-32">
+          <div className="text-white font-mono pb-8 md:pr-4">
+            <h3 className="text-xl font-bold">{">"} Inspire - KRPY X Universitas Gajah Mada (3rd Place) | 2024</h3>
+            <p className="text-md pt-2">- Organizer: Taman Pintar Yogyakarta & Universitas Gajah Mada (FMIPA, DCSE)</p>
+            <p className="text-md">- Project: Non-invasive glucometer</p>
+            <p className="text-md pt-2">- Project Background: This project addresses the growing prevalence of diabetes mellitus in children, with a focus on alleviating needle phobia by developing a non-invasive glucometer for monitoring blood glucose levels.</p>
           </div>
 
-          <div className="flex flex-col items-start justify-center mt-12 px-32">
-            <div className="text-white font-mono pb-8 md:pr-4">
-              <h3 className="text-xl font-bold">{">"} Inspire - KRPY X Universitas Gajah Mada (3rd Place) | 2024</h3>
-              <p className="text-md pt-2">- Organizer: Taman Pintar Yogyakarta & Universitas Gajah Mada (FMIPA, DCSE)</p>
-              <p className="text-md">- Project: Non-invasive glucometer</p>
-              <p className="text-md pt-2">- Project Background: This project addresses the growing prevalence of diabetes mellitus in children, with a focus on alleviating needle phobia by developing a non-invasive glucometer for monitoring blood glucose levels.</p>
-            </div>
+          <div className="text-white font-mono pb-8 md:pr-4">
+            <h3 className="text-xl font-bold">{">"} Biomedical Engineering Annual Contest - Bepro (2nd Place) | 2024</h3>
+            <p className="text-md pt-2">- Organizer: Institut Teknologi Sepuluh Nopember (FTEIC, BME)</p>
+            <p className="text-md">- Project: Non-invasive glucometer</p>
+            <p className="text-md pt-2">- Project Background: Focused on providing a solution for the high number of children with diabetes mellitus, this project features a non-invasive method for blood glucose monitoring that reduces the fear of needles.</p>
+          </div>
 
-            <div className="text-white font-mono pb-8 md:pr-4">
-              <h3 className="text-xl font-bold">{">"} Biomedical Engineering Annual Contest - Bepro (2nd Place) | 2024</h3>
-              <p className="text-md pt-2">- Organizer: Institut Teknologi Sepuluh Nopember (FTEIC, BME)</p>
-              <p className="text-md">- Project: Non-invasive glucometer</p>
-              <p className="text-md pt-2">- Project Background: Focused on providing a solution for the high number of children with diabetes mellitus, this project features a non-invasive method for blood glucose monitoring that reduces the fear of needles.</p>
-            </div>
-
-            <div className="text-white font-mono pb-8">
-              <h3 className="text-xl font-bold">{">"} Biomedical Engineering Annual Contest - Bepro (Favourite Champion) | 2024</h3>
-              <p className="text-md pt-2">- Organizer: Institut Teknologi Sepuluh Nopember (FTEIC, BME)</p>
-              <p className="text-md">- Project: Non-invasive glucometer</p>
-              <p className="text-md pt-2">- Project Background: Aimed at addressing the increasing cases of diabetes in children, this project created a non-invasive glucometer to provide an alternative to painful blood glucose testing, helping children with needle phobia.</p>
-            </div>
+          <div className="text-white font-mono pb-8">
+            <h3 className="text-xl font-bold">{">"} Biomedical Engineering Annual Contest - Bepro (Favourite Champion) | 2024</h3>
+            <p className="text-md pt-2">- Organizer: Institut Teknologi Sepuluh Nopember (FTEIC, BME)</p>
+            <p className="text-md">- Project: Non-invasive glucometer</p>
+            <p className="text-md pt-2">- Project Background: Aimed at addressing the increasing cases of diabetes in children, this project created a non-invasive glucometer to provide an alternative to painful blood glucose testing, helping children with needle phobia.</p>
           </div>
         </div>
       </div>
