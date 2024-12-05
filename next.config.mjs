f44e6c72913ad632001844feeb09b8e4b0681e82
Fakhridanish mirza Priyoyuwono <fakhridanish.mirza@gmail.com>
2024-12-05 17:55:29 +0700

    Small update

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/fakhridanishmp.github.io/' : '',
-  basePath: isProd ? '/fakhridanishmp.github.io' : '',
-  output: 'export'
 };
 
 export default nextConfig;
